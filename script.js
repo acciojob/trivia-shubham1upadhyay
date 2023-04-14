3207a9b6eb849f27a306cb951a7581be979769ea
Shubham Upadhyay <iamshubhamupadhyaya@gmail.com>
2023-04-15 00:12:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 var ans = "New Delhi is the capital of India and an administrative district of NCT Delhi.";
-
+var ans1 = "";
 function resFunc() {
  document.getElementById("res").innerHTML = ans;
 }
+function resFunc1() {
+ document.getElementById("res").innerHTML = ans1;
+}
 
 document.getElementById("nd").addEventListener("click", resFunc);
+document.getElementById("mb").addEventListener("click", resFunc1);
